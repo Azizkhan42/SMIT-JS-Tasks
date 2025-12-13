beef58121418a5ede1f599f608e4b2a96f2f0019
Aziz Ur Rehman <azizkhan43025@gmail.com>
2025-12-13 12:40:09 +0500

    files updated

--- class9.js
+++ class9.js
@@ -13,13 +13,35 @@
 // }
 
 
-var sum = 0;
-var average;
+// var sum = 0;
+// var average;
 
-var numbers = [4,5,2,5,6,8,0,1,2,8];
+// var numbers = [4,5,2,5,6,8,0,1,2,8];
 
-for(var i=0; i<numbers.length; i++){
-    console.log(numbers[i])
-     return sum + numbers
+// for(var i=0; i<numbers.length; i++){
+//     sum += numbers[i]
+
+// }
+// console.log(sum)
+
+
+// return positive and negative values from the array using function
+
+let nums = [2,4,1,4,2,-2,-4,6,3,-1,-4,8,3];
+
+
+var positive = []
+var negative = []
+
+for(var i=0; i<nums.length; i++){
+    if(nums[i]>0){
+        positive.push(nums[i])
+    }
+    else
+    {
+        negative.push(nums[i])
+    }
 
 }
+
+console.log(nums)
