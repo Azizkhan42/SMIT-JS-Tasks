dce7fa6735d8b5aec731c8bdbdd88cfbe30843b0
Aziz Ur Rehman <azizkhan43025@gmail.com>
2025-12-16 12:20:02 +0500

    files updated

--- practice.js
+++ practice.js
@@ -1,8 +1,22 @@
+var arr = [12,3,45,5,53,2];
 
-console.log("Please, Wait...", "Loading")
-setTimeout(() => {
-    console.log("Hello, Aziz")
-    
-}, 3000);
+var totals = 0;
 
-console.log("Done")
+
+for(var i = 0; i<arr.length; i++){
+    totals = totals+arr[i]
+}
+console.log(totals)
+
+var average = totals/arr.length;
+console.log(average)
+
+
+
+
+// var total = arr.reduce((totals, value)=>{
+//     return totals+value
+// })
+// console.log(total)
+
+
