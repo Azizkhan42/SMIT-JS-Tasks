5e2802fb0eb2d7abbcb17fc2df9b36fceec577cc
Aziz <azizkhan43025@gmail.com>
2025-12-24 12:15:40 +0500

    files updated

--- class12.js
+++ class12.js
@@ -1,25 +1,2 @@
-// function printVowels(str) {
-//   let count = 0;
-//   for (let val of str) {
-//     if (
-//       val === "a" ||
-//       val === "e" ||
-//       val === "i" ||
-//       val === "o" ||
-//       val === "u"
-//     ) {
-//       count++;
-//     }
-//   }
-//   console.log(count);
-// }
-
-// printVowels("This is function for vowels finding");
 
 
-
-let arr = [1,2,3,4,5];
-arr.forEach((val)=>{
-    console.log(val*val)
-
-})
--- practice.js
+++ practice.js
@@ -1,7 +1,40 @@
+// function printVowels(str) {
+//   let count = 0;
+//   for (let val of str) {
+//     if (
+//       val === "a" ||
+//       val === "e" ||
+//       val === "i" ||
+//       val === "o" ||
+//       val === "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// }
+
+// printVowels("This is function for vowels finding");
 
 
-console.log(name())
 
-function name(){
-    return "Aziz"
-}
+// let arr = [1,2,3,4,5];
+// arr.forEach((val)=>{
+//     console.log(val*val)
+
+// })
+
+// function palindrom(str){
+//     let reversed = str.split('').reverse().join('')
+//     return str === reversed
+// }
+
+// console.log(palindrom("aziz"))
+
+
+// function titleCase(str){
+//     let toUpperCase = str.toUpperCase();
+//     console.log(toUpperCase)
+// }
+
+// titleCase("aziz")
