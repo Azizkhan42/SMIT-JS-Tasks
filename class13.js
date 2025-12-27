23943cba38d62fa22c4887b6c4753fd15584eb03
Aziz <azizkhan43025@gmail.com>
2025-12-27 22:52:36 +0500

    files updated

--- class13.js
+++ class13.js
@@ -1,7 +1,7 @@
-let arr = [1,2,3,4,5,6,7,8,9];
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12];
 
-let doubleArrayValues = arr.reduce((add, data)=>{
-    return  add + data;
-},0)
+let array = arr.forEach((values)=>{
+     console.log(values*2)
+})
 
-console.log(doubleArrayValues)
+console.log(arr)
