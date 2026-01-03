6f01f652f3962ede78d02250198f1add560c4a39
Azizkhan42 <azizkhan43025@gmail.com>
2026-01-03 13:49:52 +0500

    files updated

--- practice.js
+++ practice.js
@@ -1,53 +1,7 @@
-// function printVowels(str) {
-//   let count = 0;
-//   for (let val of str) {
-//     if (
-//       val === "a" ||
-//       val === "e" ||
-//       val === "i" ||
-//       val === "o" ||
-//       val === "u"
-//     ) {
-//       count++;
-//     }
-//   }
-//   console.log(count);
-// }
+const names = [2,4,6,3,8,54,34,15,3,1,7,0,5]
 
-// printVowels("This is function for vowels finding");
-
-
-
-// let arr = [1,2,3,4,5];
-// arr.forEach((val)=>{
-//     console.log(val*val)
-
-// })
-
-// function palindrom(str){
-//     let reversed = str.split('').reverse().join('')
-//     return str === reversed
-// }
-
-// console.log(palindrom("aziz"))
-
-
-// function titleCase(str){
-//     let toUpperCase = str.toUpperCase();
-//     console.log(toUpperCase)
-// }
-
-// titleCase("aziz")
-
-// let arr = [1,2,3,4,5,6];
-
-// function reverseArr(arr){
-//     let reversed = arr.reverse()
-//     console.log(reversed)
-// }
-// reverseArr(arr)
-
-
-
-let num = "abc" - 4
-console.log(num)
+let sum = 0;
+names.forEach((data)=>{
+  sum = sum + data;
+})
+console.log(sum)  
