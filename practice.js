4a58e08ea3221c5ea858ceec16cb2afeb66e903d
Azizkhan42 <azizkhan43025@gmail.com>
2026-01-06 23:01:37 +0500

    files updated

--- practice.js
+++ practice.js
@@ -1,10 +1,26 @@
-function parent(){
-  let name = "Aziz";
-
-  function child(){
-    console.log(name)
-  }
-  child()
+function learn(cb){
+  setTimeout(() => {
+    console.log("Learn")
+    cb()
+  }, 1000);
 }
 
-parent()
+function done(){
+  setTimeout(() => {
+    console.log("Done")
+  }, 5000);
+}
+
+function Earn(cb){
+  setTimeout(() => {
+    console.log("Earn")
+    cb()
+  }, 2000);
+}
+
+learn(()=>{
+  Earn(()=>{
+    done()
+  })
+})
+
