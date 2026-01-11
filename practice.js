b6d5e305b0c27d9186b9d622edd7e0490188cd3d
Azizkhan42 <azizkhan43025@gmail.com>
2026-01-11 20:47:52 +0500

    files updated

--- practice.js
+++ practice.js
@@ -1,17 +1,7 @@
-var users = [
-  { name: "Alice", age: 22 },
-  { name: "Foo", age: 26 },
-  { name: "Bob", age: 30 },
-  { name: "Charlie", age: 25 },
-];
+let arr = [1, 4, 9, 16];
 
-let oldAge = users.filter((item)=>{
-  return item.age > 25;
-})
+let sum = arr.map((data) => {
+  return data / data;
+});
 
-// console.log(oldAge)
-
-let names = users.map((item)=>{
-  return item.name;
-})
-console.log(names)
+console.log(sum);
