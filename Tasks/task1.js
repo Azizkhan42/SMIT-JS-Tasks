2778eafb87f061c1d08922077c4f9859ca13a7ee
Azizkhan42 <azizkhan43025@gmail.com>
2026-01-12 12:10:06 +0500

    input from user program added

---
+++ Tasks/task1.js
@@ -0,0 +1,26 @@
+/* Qs. Take a number n as input from user. Create an array of numbers from 1 to n.
+        Use the reduce method to calculate sum of all numbers in the array.
+        Use the reduce method to calculate product of all numbers in the array.   
+*/
+
+let inp = +prompt("Enter the number : ")
+
+let arr = [];
+
+for(let i = 1; i<=inp; i++){
+  arr[i-1] = i  // 1-1 = 0, 2-1 = 1, 3-1=2; ---> store at index
+}
+
+console.log(arr)
+
+let output = arr.reduce((result, val)=>{
+  return result + val
+})
+
+console.log("Sum : ", output)
+
+let fact = arr.reduce((result, val)=>{
+  return result * val
+})
+
+console.log("factorial : ", fact)
--- practice.js
+++
@@ -1,7 +0,0 @@
-let marks = [22,55,76,99,89,78,99,90];
-
-let result = marks.filter((value)=>{
-  return value > 90;
-})
-
-console.log(result)
