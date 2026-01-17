679361c38d9721027c657e73078af0c46c0d5abd
Azizkhan42 <azizkhan43025@gmail.com>
2026-01-17 11:18:55 +0500

    files updated

--- practice.js
+++ practice.js
@@ -1,5 +1,11 @@
+// let price = 499.99;
+// let tax = 5;
+
+// let total = price + (price * tax / 100);
+// console.log(Math.round(total));
+
 let date = new Date();
 
-console.log(date.getMonth())
-
-
+setInterval(() => {
+  console.log(date.getSeconds());
+}, 1000);
