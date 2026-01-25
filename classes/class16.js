bee6ec221eaf7c03537f56ea2daf3d51a7c54731
Azizkhan42 <azizkhan42025@gmail.com>
2026-01-25 08:57:21 +0500

    files updated

--- classes/class16.js
+++ classes/class16.js
@@ -1,2 +1,24 @@
-let dt = new Date();
-console.log(dt.getHours())
+let seat = [
+    ["A", "B", "C", "D", "E", "F"],
+    ["A", "B", "C", "D", "E", "F"],
+    ["A", "B", "C", "D", "E", "F"],
+    ["A", "B", "C", "D", "E", "F"],
+    ["A", "B", "C", "D", "E", "F"],
+    ["A", "B", "C", "D", "E", "F"],
+    ["A", "B", "C", "D", "E", "F"],
+    ["A", "B", "C", "D", "E", "F"],
+    ["A", "B", "C", "D", "E", "F"],
+]
+
+for(let i=0; i<seat.length; i++){
+    const element = seat[i];
+
+    for(let j=0; j<element.length; j++){
+        const seatNumber = i +1 + element[j];
+        if(seatNumber==="4C" || seatNumber ==="3D")
+        {
+            console.log("Muhammad Osama you got the seat")
+        }
+        console.log(seatNumber)
+    }
+}
