c842e4dfd4cb1248d5e39c6b6b915ba687efc6f4
Azizkhan42 <azizkhan43025@gmail.com>
2026-01-31 12:04:16 +0500

    files updated

--- practice.js
+++ practice.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+let arr = [true, 0, false, "", undefined, 1, " ", null, 22]
+
+let print = arr.filter((val)=>{
+    if(val>0 || " "){
+        return val;
+    } 
+})
+
+console.log(print)
