ef4e5ea8c399ad761b6a1a289a32216fecd6ccf9
Azizkhan42 <azizkhan43025@gmail.com>
2026-05-30 20:53:32 +0500

    for-in | for-of | Objects | Arrays

--- practice.js
+++ practice.js
@@ -12,16 +12,40 @@
 
 // console.log(msg + name)
 
-let stdMarks = [78,87,69,36,46,37,87,90];
+// let stdMarks = [78,87,69,36,46,37,87,90];
 
-function percentage(){
-    let sum = stdMarks.reduce((acc, add)=>acc + add)
-    let percent = sum/800 * 100;
-    console.log(percent)
-}
+// function percentage(){
+//     let sum = stdMarks.reduce((acc, add)=>acc + add)
+//     let percent = sum/800 * 100;
+//     console.log(percent)
+// }
 
-percentage()
-
-
+// percentage()
 
 
+// console.log(name)
+// var name = "Aziz";
+
+
+
+
+// const user = {
+//     name: "Aziz",
+//     id: 15465,
+//     idprint: function print(){
+//     console.log("your id is : ", this.id)
+// }
+// }
+
+// for(let value in user){
+//     console.log(user[value])
+// }
+
+
+// let arr = [1,2,3,4,5]
+
+// let str = "Aziz"
+
+// for(let values of str){
+//     console.log(values)
+// }
