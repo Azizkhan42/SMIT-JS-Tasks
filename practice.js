f6ca26885e884e0da03c2fddac700ce4b1d4d9b2
Azizkhan42 <azizkhan43025@gmail.com>
2026-07-02 15:58:34 +0500

    files updated

--- practice.js
+++ practice.js
@@ -1,9 +1,9 @@
-function addition(){
-    let numOne = +prompt("Enter number one")
-    let numTwo = +prompt("Enter number two")
+let js = "Nested For Loop in Js";
+console.log(js)
 
-    console.log(numOne + numTwo)
-    
+for(let i=1; i<=3; i++){
+    let row = ""
+    for(let j=1; j<=3; j++){
+        console.log(i)
+    }
 }
-
-addition()
