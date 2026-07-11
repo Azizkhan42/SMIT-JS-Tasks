72e63fc6ca700c5a5fe4b5b39cccc920149573a2
Azizkhan42 <azizkhan43025@gmail.com>
2026-07-11 16:08:48 +0500

    Practice object literals

--- practice.js
+++ practice.js
@@ -3,18 +3,20 @@
 // }
 // console.log(name instanceof Object)
 
-let user = {
-    name: "Aziz",
-    age: 22,
-    city: "Peshawar",
-    notice: function(){
-        console.log(`Hello, ${user.name}  you are eligible for this role.`)
-    }
-};
+// let user = {
+//     name: "Aziz",
+//     age: 22,
+//     city: "Peshawar",
+//     notice: function(){
+//         console.log(`Hello, ${user.name}  you are eligible for this role.`)
+//     }
+// };
 
-const {name, age, city} = user;
+// const {name, age, city} = user;
 
-console.log(name, age, city)
+// console.log(name, age, city)
 // for(const values in user){
 //     console.log(user[values])
 // }
+
+
