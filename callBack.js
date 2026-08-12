a658a63406002811ddbc9064f8ceaf210af6eff2
Azizkhan42 <azizkhan43025@gmail.com>
2026-08-12 23:55:18 +0500

    practice js

--- callBack.js
+++ callBack.js
@@ -1,22 +1,5 @@
-function name(){
-    console.log("Aziz")
-}
-console.log(name instanceof Object)
-
-let user = {
-    name: "Aziz",
-    age: 22,
-    city: "Peshawar",
-    notice: function(){
-        console.log(`Hello, ${user.name}  you are eligible for this role.`)
-    }
-};
-
-const {name, age, city} = user;
-
-console.log(name, age, city)
-for(const values in user){
-    console.log(user[values])
+function addition(a, b, ...rest) {
+  console.log(a + b);
 }
 
-
+addition(2, 3, 4);
--- practice.js
+++ practice.js
@@ -1,22 +1,22 @@
-function name(){
-    console.log("Aziz")
-}
-console.log(name instanceof Object)
+// function name(){
+//     console.log("Aziz")
+// }
+// console.log(name instanceof Object)
 
-let user = {
-    name: "Aziz",
-    age: 22,
-    city: "Peshawar",
-    notice: function(){
-        console.log(`Hello, ${user.name}  you are eligible for this role.`)
-    }
-};
+// let user = {
+//     name: "Aziz",
+//     age: 22,
+//     city: "Peshawar",
+//     notice: function(){
+//         console.log(`Hello, ${user.name}  you are eligible for this role.`)
+//     }
+// };
 
-const {name, age, city} = user;
+// const {name, age, city} = user;
 
-console.log(name, age, city)
-for(const values in user){
-    console.log(user[values])
-}
+// console.log(name, age, city)
+// for(const values in user){
+//     console.log(user[values])
+// }
 
 
