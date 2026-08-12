ed187f144f8a413917c77edb1f7dcc36308c5329
Azizkhan42 <azizkhan43025@gmail.com>
2026-08-12 23:54:45 +0500

    files updated

--- callBack.js
+++ callBack.js
@@ -1,5 +1,22 @@
-function addition(a, b, ...rest) {
-  console.log(a + b);
+function name(){
+    console.log("Aziz")
+}
+console.log(name instanceof Object)
+
+let user = {
+    name: "Aziz",
+    age: 22,
+    city: "Peshawar",
+    notice: function(){
+        console.log(`Hello, ${user.name}  you are eligible for this role.`)
+    }
+};
+
+const {name, age, city} = user;
+
+console.log(name, age, city)
+for(const values in user){
+    console.log(user[values])
 }
 
-addition(2, 3, 4);
+
